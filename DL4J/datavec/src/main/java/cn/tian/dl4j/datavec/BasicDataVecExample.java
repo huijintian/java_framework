bc5a638452a30649336fea1211b010db43941fbc
mengtian <wu750316@163.com>
2018-04-02 15:06:24 +0800

    add

---
+++ DL4J/datavec/src/main/java/cn/tian/dl4j/datavec/BasicDataVecExample.java
@@ -0,0 +1,7 @@
+package cn.tian.dl4j.datavec;
+
+/**
+ * Created by mengtian on 2018/3/28
+ */
+public class BasicDataVecExample {
+}
---
+++ DL4J/datavec/src/main/java/cn/tian/dl4j/datavec/BasicTransformExample.java
@@ -0,0 +1,7 @@
+package cn.tian.dl4j.datavec;
+
+/**
+ * Created by mengtian on 2018/3/30
+ */
+public class BasicTransformExample {
+}
