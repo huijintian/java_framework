d047f5bba9efb661e4ec87464db616b0b004fd61
huijintian <wu750316@163.com>
2018-09-11 00:30:47 +0800

    add method to redisString

--- redis/src/main/java/cn/tian/redis/string/RedisString.java
+++ redis/src/main/java/cn/tian/redis/string/RedisString.java
@@ -54,6 +54,51 @@ public class RedisString {
         System.out.println(jedis.getSet(key, "mysql"));
     }
 
+    public void incr() {
+        System.out.println("incr>>>>>");
+        String key = "page_view";
+        //init value
+        System.out.println(jedis.set(key, "20"));
+        System.out.println(jedis.incr(key));
+    }
+
+    /**
+     * use redis incr to controller user api caller times
+     */
+    public void apiCallLimit(String ip) {
+        //current seconds
+        long time = System.currentTimeMillis() / 1000;
+        String key = ip + ":" + time;
+
+        String currentCount = jedis.get(key);
+        Integer current = currentCount == null ? null : Integer.parseInt(currentCount);
+
+        if (current == null) {
+            jedis.set(key, "1");
+            jedis.expire(key, 1);
+        } else if (current < 10) {
+            jedis.incr(key);
+        } else if (current >= 10) {
+            //some controller action
+            System.err.println("too many requests per second");
+        }
+    }
+
+    public void mSetGet() {
+        System.out.println("mGet>>>>>");
+        jedis.mset("redis", "redis.com", "mongodb", "mongodb.org");
+        jedis.mget("redis", "mongodb").forEach(s -> System.out.println(s));
+    }
+
+    public void mSetNx() {
+        System.out.println("mSetNx>>>>>");
+        jedis.msetnx("mysql", "mysql.com", "java", "java.com");
+        jedis.mget("mysql", "java").forEach(s -> System.out.println(s));
+        Long msetnx = jedis.msetnx("sqlserver", "sqlserver.com", "mysql", "mysql.com");
+        System.out.println("status:" + msetnx);
+        System.out.println(jedis.get("sqlserver"));
+    }
+
     public static void main(String[] args) {
         jedis.flushAll();
         RedisString redisString = new RedisString();
@@ -62,6 +107,13 @@ public class RedisString {
         redisString.decr();
         redisString.decrby();
         redisString.getSet();
+        redisString.incr();
+        redisString.apiCallLimit("local");
+        for (int i = 0; i < 20; i++) {
+            redisString.apiCallLimit("local");
+        }
+        redisString.mSetGet();
+        redisString.mSetNx();
         jedis.close();
     }
 }
